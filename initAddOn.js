16f74890af2cfe4c54c05dec7c73fd1609680bd6
Chiara <chiara.biancacci@instathings.io>
2020-01-28 14:44:41 +0100

    trycatch

--- initAddOn.js
+++ initAddOn.js
@@ -8,26 +8,33 @@ module.exports = function initAddOn(addOn, deviceType) {
       break;
 
     default:
-      const ls = spawn('npm', ['i', addOn]);
-
-      ls.stdout.on("data", data => {
-        console.log(`stdout: ${data}`);
-      });
-
-      ls.stderr.on("data", data => {
-        console.log(`stderr: ${data}`);
-      });
-
-      ls.on('error', (error) => {
-        console.log(`error: ${error.message}`);
-      });
-
-      ls.on('close', code => {
-        console.log(`child process exited with code ${code}`);
+      // sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
+      try {
         const GateAddOnSensorTag = require(addOn);
         const gAddSensorTag = new GateAddOnSensorTag();
         gAddSensorTag.start();
-      });
+      } catch (error) {
+        const ls = spawn('npm', ['i', addOn]);
+
+        ls.stdout.on("data", (data) => {
+          console.log(`stdout: ${data}`);
+        });
+
+        ls.stderr.on("data", (data) => {
+          console.log(`stderr: ${data}`);
+        });
+
+        ls.on('error', (error) => {
+          console.log(`error: ${error.message}`);
+        });
+
+        ls.on('close', (code) => {
+          console.log(`child process exited with code ${code}`);
+          const GateAddOnSensorTag = require(addOn);
+          const gAddSensorTag = new GateAddOnSensorTag();
+          gAddSensorTag.start();
+        });
+      }
       break;
   }
 };

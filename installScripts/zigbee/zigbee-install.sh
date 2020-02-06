ee55a6b6fa58171957339e9c437b6c9294c03aed
Chiara <chiara.biancacci@instathings.io>
2020-02-06 15:54:22 +0100

    fix install script zigbee

--- installScripts/zigbee/zigbee-install.sh
+++ installScripts/zigbee/zigbee-install.sh
@@ -2,11 +2,11 @@
 cd /home/gate/service
 mkdir mosquitto && cd mosquitto
 mkdir config && mkdir data && mkdir log
-cp /home/gate/installScripts/zigbee/mosquitto/mosquitto.conf /home/gate/service/config
+cp /home/gate/installScripts/zigbee/mosquitto/mosquitto.conf /home/gate/service/mosquitto/config
 # mosquitto container
-docker run -it \
+docker run -dt \
 -p 1883:1883 -p 9001:9001 \
--v /home/gate/service/mosquitto/config:/mosquitto/config \
--v /home/gate/service/mosquitto/data:/mosquitto/data \
--v /home/gate/service/mosquitto/log:/mosquitto/log \
+-v ~/service/mosquitto/config:/mosquitto/config \
+-v ~/service/mosquitto/data:/mosquitto/data \
+-v ~/service/mosquitto/log:/mosquitto/log \
  eclipse-mosquitto

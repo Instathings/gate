401f8d92e8bd628b09f4da17b9d99a27675f1ade
Chiara <chiara.biancacci@instathings.io>
2020-03-20 10:29:46 +0100

    send versioning fix

--- installProtocol.js
+++ installProtocol.js
@@ -4,7 +4,7 @@ const _ = require('lodash');
 const pjson = require('./package.json');
 
 const docker = new Docker();
-const gatewayVersion = {};
+const gwSoftwaresVersions = {};
 const gateVersion = pjson.version;
 
 
@@ -19,11 +19,11 @@ module.exports = function installProtocol(topic, protocol, protocolId, device) {
       containers.forEach((container) => {
         let containerName = container.Names;
         containerName = containerName[0].replace('/', '');
-        _.set(gatewayVersion, `${containerName}`, container.Image);
+        _.set(gwSoftwaresVersions, `${containerName}`, container.Image);
         if (containerName === 'gate') {
           let gateImage = container.Image;
           gateImage = gateImage.replace('latest', gateVersion);
-          _.set(gatewayVersion, 'gate', gateImage);
+          _.set(gwSoftwaresVersions, 'gate', gateImage);
         }
       });
     });
@@ -35,7 +35,7 @@ module.exports = function installProtocol(topic, protocol, protocolId, device) {
       installationSuccess: code === 0,
       stdout,
       stderr,
-      gateVersion,
+      gwSoftwaresVersions,
     };
     const responseTopic = topic.replace('/post', '');
     device.publish(responseTopic, JSON.stringify(installResponse));

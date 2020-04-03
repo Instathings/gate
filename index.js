2b6668b92135868bdf52321ff1274e7edf91808f
Claudio Petrini <claudio.petrini@instathings.io>
2020-04-03 11:24:17 +0200

    Handling upgrade

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv');
 
-const Gate = require('./lib/Gate');
-
 const configFolder = process.env.CONFIG_FOLDER || 'config';
 const configFile = `${__dirname}/${configFolder}/.env`;
 dotenv.config({ path: configFile });
+
+const Gate = require('./lib/Gate');
 
 const gate = new Gate(configFolder);
 gate.start();

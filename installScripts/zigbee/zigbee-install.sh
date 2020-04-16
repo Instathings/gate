e772398f359e6c57c86e1ea6819a676dcea73cd2
Claudio Petrini <claudio.petrini@instathings.io>
2020-04-16 12:19:34 +0200

    fix build, fix zigbee install

--- installScripts/zigbee/zigbee-install.sh
+++ installScripts/zigbee/zigbee-install.sh
@@ -1,25 +1,31 @@
-# mosquitto configuration
-cd /home/node/gate/service
-mkdir mosquitto
-cd mosquitto
-mkdir config
-mkdir data
-mkdir log
-chmod -R 777 config
-chmod -R 777 data
-chmod -R 777 log
-cp /home/node/gate/installScripts/zigbee/mosquitto/mosquitto.conf /home/node/gate/service/mosquitto/config
-# mosquitto container
-docker run -dt \
--p 1883:1883 -p 9001:9001 \
--v /home/$HOST_USERNAME/service/mosquitto/config:/mosquitto/config \
--v /home/$HOST_USERNAME/service/mosquitto/data:/mosquitto/data \
--v /home/$HOST_USERNAME/service/mosquitto/log:/mosquitto/log \
---name eclipse-mosquitto \
---network gate-net \
---restart=always \
- eclipse-mosquitto:1.6.9
+CONTAINER_NAME='eclipse-mosquitto'
 
+CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
+
+# Check if a container named eclipse-mosquitto is running
+if [ ! "${CID}" ]; then
+   # mosquitto configuration
+   cd /home/node/gate/service
+   mkdir mosquitto
+   cd mosquitto
+   mkdir config
+   mkdir data
+   mkdir log
+   chmod -R 777 config
+   chmod -R 777 data
+   chmod -R 777 log
+   cp /home/node/gate/installScripts/zigbee/mosquitto/mosquitto.conf /home/node/gate/service/mosquitto/config
+   # mosquitto container
+   docker run -dt \
+   -p 1883:1883 -p 9001:9001 \
+   -v /home/$HOST_USERNAME/service/mosquitto/config:/mosquitto/config \
+   -v /home/$HOST_USERNAME/service/mosquitto/data:/mosquitto/data \
+   -v /home/$HOST_USERNAME/service/mosquitto/log:/mosquitto/log \
+   --name eclipse-mosquitto \
+   --network gate-net \
+   --restart=always \
+   eclipse-mosquitto:1.6.9
+fi
 # zigbee2mqtt
 cd /home/node/gate/service
 mkdir zigbee2mqtt

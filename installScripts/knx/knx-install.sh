80d96d3d3fe23f3cea58ec0596975441c8924431
Chiara <chiara.biancacci@instathings.io>
2020-04-23 17:30:45 +0200

    knx install script

---
+++ installScripts/knx/knx-install.sh
@@ -0,0 +1,47 @@
+CONTAINER_NAME='eclipse-mosquitto'
+
+CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$)
+# Check if a container named eclipse-mosquitto is running
+if [ ! "${CID}" ]; then
+   cd /home/node/gate/service
+   mkdir mosquitto
+   cd mosquitto
+   mkdir config
+   mkdir data
+   mkdir log
+   chmod -R 777 config
+   chmod -R 777 data
+   chmod -R 777 log
+   cp /home/node/gate/installScripts/zigbee/mosquitto/mosquitto.conf /home/node/gate/service/mosquitto/config  
+
+   docker run -dt \
+   -p 1883:1883 -p 9001:9001 \
+   -v /home/$HOST_USERNAME/service/mosquitto/config:/mosquitto/config \
+   -v /home/$HOST_USERNAME/service/mosquitto/data:/mosquitto/data \
+   -v /home/$HOST_USERNAME/service/mosquitto/log:/mosquitto/log \
+   --name eclipse-mosquitto \
+   --network gate-net \
+   --restart=always \
+   eclipse-mosquitto:1.6.9
+fi
+
+# knx
+cd /home/node/gate/service
+mkdir knx
+chmod -R 777 knx
+cd knx
+mkdir data
+chmod -R 777 data
+cp /home/node/gate/installScripts/knx/knx2mqtt/configuration.yaml /home/node/gate/service/knx/data/configuration.yaml
+
+docker run \
+-dt \
+-v /home/$HOST_USERNAME/service/knx/data:/app/data \
+-e TZ=GMT \
+-v /run/udev:/run/udev:ro \
+--privileged=true \
+--name instathings-knx2mqtt \
+--network gate-net \
+--restart=always \
+instathings/knx2mqtt:staging
+
